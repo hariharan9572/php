b03d536de6b570d7c4109652339a707d16c1653d
Hariharan <hariharan9572@gmail.com>
2025-08-28 10:32:06 +0530

    switch & match

---
+++ switch.php
@@ -0,0 +1,36 @@
+<?php
+
+$size = "L";
+
+switch($size){
+    case "S":
+    case "M":
+        echo "Small or Medium size\n";
+        break;
+    case "L":
+    case "XL":
+        echo "Large or Extra Large size\n";
+        break;
+    default:
+    echo "Unknown size\n";
+}
+
+
+if ("S" == $size || "M" == $size){
+    echo "Small or Medium size\n";
+} elseif ("L" == $size || "XL" == $size){
+    echo "Large or Extra Large size\n";
+} else {
+    echo "Unknown size\n";
+}
+
+
+$badAttempts = 3;
+
+switch($badAttempts){
+    case 3:
+        echo "You are blocked!\n";
+    
+    default:
+        echo "Bad attempt detected!\n";
+}
